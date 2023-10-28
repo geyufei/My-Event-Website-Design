8ed148dc15f0fca818a79bcb1bbe9315a2836b4e
Yufei1219 <141001788+geyufei@users.noreply.github.com>
2023-10-28 17:49:19 +1000

    update
    new change

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,18 +1,16 @@
+ // Display the loading animation initially
+ document.getElementById("loading-spinner").style.display = "block";
 
-// start
-// Display the loading animation initially
-document.getElementById("loading-spinner").style.display = "block";
-
-// After a delay, hide the loading animation and show the content with a fade-in effect
-setTimeout(function () {
-    var content = document.getElementById("login-form");
-    content.style.display = "block";
-    content.style.opacity = 0;
-    var fadeInInterval = setInterval(function () {
-        content.style.opacity = parseFloat(content.style.opacity) + 0.1;
-        if (parseFloat(content.style.opacity) >= 1) {
-            clearInterval(fadeInInterval);
-        }
-    }, 100); // Adjust the interval duration as needed
-    document.getElementById("loading-spinner").style.display = "none";
-}, 500); // Adjust the delay (in milliseconds) as needed
+ // After a delay, hide the loading animation and show the content with a fade-in effect
+ setTimeout(function () {
+   var content = document.getElementById("login-form");
+   content.style.display = "block";
+   content.style.opacity = 0;
+   var fadeInInterval = setInterval(function () {
+     content.style.opacity = parseFloat(content.style.opacity) + 0.1;
+     if (parseFloat(content.style.opacity) >= 1) {
+       clearInterval(fadeInInterval);
+     }
+   }, 100); // Adjust the interval duration as needed
+   document.getElementById("loading-spinner").style.display = "none";
+ }, 500); // Adjust the delay (in milliseconds) as needed 
